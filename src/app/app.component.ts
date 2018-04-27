4c8b560662245ec25f3adfd80c0167b83e61eb4b
hewenhao-hwh <282106931@qq.com>
2018-04-27 23:18:31 +0800

    Tesseract+OCR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  img_url="https://placehold.it/550x550";
+  onSelect(): void {
+    $("#invisiable").click();
+  }
+  onChange(): void {
+    var src=($("#invisiable")[0] as HTMLInputElement).value;
+    alert(src);
+    $("#target_img").attr("src",src);
+    //const img1 = require('../assets/img1.png');
+  }
 }
